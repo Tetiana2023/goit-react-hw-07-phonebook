6ab4b912a32ab6aadeac953b91393b1a398871aa
Tetiana2023 <drofaj18@gmail.com>
2023-04-21 00:02:36 +0300

    3

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -4,6 +4,10 @@ import { fetchContacts, addContact, deleteContact } from './operations';
 const handlePending = state => {
   state.isLoading = true;
 };
+const handleFulfilled = state => {
+  state.isLoading = false;
+  state.error = null;
+};
 
 const handleRejected = (state, action) => {
   state.isLoading = false;
@@ -19,32 +23,34 @@ export const contactsSlice = createSlice({
   },
   ExtraReducers: {
     [fetchContacts.pending]: handlePending,
-    [fetchContacts.fulfilled]: (state, action) =>{
-        state.isLoading = false;
-        state.error = null;
-        state.items = action.payload;
-      },
-      [fetchContacts.rejected]: handleRejected,
+    [fetchContacts.fulfilled]: (state, action) => {
+      state.isLoading = false;
+      state.error = null;
+      state.items = action.payload;
+      handleFulfilled(state);
+    },
+    [fetchContacts.rejected]: handleRejected,
 
-      [addContact.pending]: handlePending,
-      [addContact.fulfilled]:(state, action) =>{
-        state.isLoading = false;
-        state.error = null;
-        state.items.push(action.payload);
-      },
-      [addContact.rejected]:handleRejected,
+    [addContact.pending]: handlePending,
+    [addContact.fulfilled]: (state, action) => {
+      state.isLoading = false;
+      state.error = null;
+      state.items.push(action.payload);
+      handleFulfilled(state);
+    },
+    [addContact.rejected]: handleRejected,
 
-      [deleteContact.pending]:handlePending,
-      [deleteContact.fulfilled]:(state, action)=> {
-        state.isLoading = false;
-        state.error = null;
-        state.items= state.items.filter(contact => contact.id !== action.payload)
-      },
-      [deleteContact.rejected]: handleRejected,
-
+    [deleteContact.pending]: handlePending,
+    [deleteContact.fulfilled]: (state, action) => {
+      state.isLoading = false;
+      state.error = null;
+      state.items = state.items.filter(
+        contact => contact.id !== action.payload
+      );
+      handleFulfilled(state)
+    },
+    [deleteContact.rejected]: handleRejected,
   },
 });
 
 export const phonebookReducer = contactsSlice.reducer;
-
-
--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,9 +1,10 @@
 import { createSelector } from "@reduxjs/toolkit";
 
 export const getContacts = state => state.phonebook.items;
-export const getFilter = state => state.phonebook.filter;
 export const getError = state => state.phonebook.error;
 export const getIsLoading = state => state.phonebook.isLoadding;
+
+export const getFilter = state => state.filter;
 
 export const getFiltredContacts = createSelector (
     [getContacts, getFilter], (contacts, filter )=> contacts.filter(contact =>
